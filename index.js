17a7804dc207bfc5798584319b63ffa0901fa2b7
David Purdy <dave.purdy1@gmail.com>
2020-10-07 02:09:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var animal = 'dog'
 
 function myAnimal() {
   // You should not need to modify this function
+  var myAnimal = 'dog';
   return animal
 }
 
@@ -11,7 +12,9 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
+  var animal = 'cat';
   return animal
+  
 }
 
 function add2(n) {
